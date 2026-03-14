e476eb5f023317fed500a585252d294376a8999e
Julian Nguyen <huutrong.160705@gmail.com>
2026-03-14 08:58:48 +0700

    add model

---
+++ TH_API/src/main/java/julian/APIPageController.java
@@ -0,0 +1,19 @@
+package julian;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIPageController {
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
+
+	@PostMapping("/api/hello")
+	public String helloPost(@RequestBody String body) {
+		return "Noi dung nhan duoc: " + body;
+	}
+}
