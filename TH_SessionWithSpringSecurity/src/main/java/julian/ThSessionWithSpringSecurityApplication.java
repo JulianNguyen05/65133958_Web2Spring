8e2be40bc8bb293f512e784059162bd9c119377b
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-01 16:32:07 +0700

    session with security

---
+++ TH_SessionWithSpringSecurity/src/main/java/julian/ThSessionWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package julian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThSessionWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThSessionWithSpringSecurityApplication.class, args);
+	}
+
+}
