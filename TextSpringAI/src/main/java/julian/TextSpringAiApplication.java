a6288c56ef9526b4794d404334add783fd0eab73
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-01 14:07:24 +0700

    intel project

---
+++ TextSpringAI/src/main/java/julian/TextSpringAiApplication.java
@@ -0,0 +1,13 @@
+package julian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextSpringAiApplication.class, args);
+	}
+
+}
---
+++ TextSpringAI/src/test/java/julian/TextSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package julian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
