afc3c9f18b005a8a78ee191da11292a0fc0e4220
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-07 20:23:06 +0700

    update repository

--- OnThiGK_SpringBoot/src/main/java/julian/repository/StudentRepository.java
+++ OnThiGK_SpringBoot/src/main/java/julian/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package julian.repository;
 
-public class StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import julian.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
 }
---
+++ OnThiGK_SpringBoot/src/main/java/julian/repository/TopicRepository.java
@@ -0,0 +1,10 @@
+package julian.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import julian.model.Topic;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, String> {
+}
