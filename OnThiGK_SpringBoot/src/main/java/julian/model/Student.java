e647d10fb0076bb2d3a29fa77e31eaa14df79d06
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-07 19:25:30 +0700

    update model

--- OnThiGK_SpringBoot/src/main/java/julian/model/Student.java
+++ OnThiGK_SpringBoot/src/main/java/julian/model/Student.java
@@ -1,5 +1,14 @@
 package julian.model;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+	private String id;
+	private String name;
+	private String groupId;
 }
--- OnThiGK_SpringBoot/src/main/java/julian/model/Topic.java
+++ OnThiGK_SpringBoot/src/main/java/julian/model/Topic.java
@@ -1,5 +1,16 @@
 package julian.model;
 
-public class Topic {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topic {
+	private String id;
+	private String topicName;
+	private String topicDescription;
+	private String supervisorId;
+	private String topicType;
 }
