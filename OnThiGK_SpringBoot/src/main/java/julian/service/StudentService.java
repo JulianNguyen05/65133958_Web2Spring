3984f8050b8b253e82d44e12de11588eeb292ac2
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-07 20:27:11 +0700

    update service

--- OnThiGK_SpringBoot/src/main/java/julian/service/StudentService.java
+++ OnThiGK_SpringBoot/src/main/java/julian/service/StudentService.java
@@ -1,5 +1,32 @@
 package julian.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import julian.model.Student;
+import julian.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class StudentService {
 
+	private final StudentRepository studentRepository;
+
+	public List<Student> getAll() {
+		return studentRepository.findAll();
+	}
+
+	public Student getById(String id) {
+		return studentRepository.findById(id).orElse(null);
+	}
+
+	public void add(Student student) {
+		studentRepository.save(student);
+	}
+
+	public void delete(String id) {
+		studentRepository.deleteById(id);
+	}
 }
--- OnThiGK_SpringBoot/src/main/java/julian/service/TopicService.java
+++ OnThiGK_SpringBoot/src/main/java/julian/service/TopicService.java
@@ -1,5 +1,32 @@
 package julian.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import julian.model.Topic;
+import julian.repository.TopicRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class TopicService {
 
+	private final TopicRepository topicRepository;
+
+	public List<Topic> getAll() {
+		return topicRepository.findAll();
+	}
+
+	public Topic getById(String id) {
+		return topicRepository.findById(id).orElse(null);
+	}
+
+	public void add(Topic topic) {
+		topicRepository.save(topic);
+	}
+
+	public void delete(String id) {
+		topicRepository.deleteById(id);
+	}
 }
