cd4c26c2abed79c1cdab2514daedc434ba4d40b8
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-16 17:15:54 +0700

    Create project package structure

---
+++ OnThiGK_QLTinTuc/src/main/java/julian/OnThiGkQlTinTucApplication.java
@@ -0,0 +1,13 @@
+package julian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnThiGkQlTinTucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnThiGkQlTinTucApplication.class, args);
+	}
+
+}
