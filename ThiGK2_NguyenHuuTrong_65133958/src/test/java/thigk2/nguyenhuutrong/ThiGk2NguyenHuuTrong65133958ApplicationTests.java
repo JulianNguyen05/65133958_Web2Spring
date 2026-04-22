de6d3363597d2eb8e3c1157159420d5cc36cddd0
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-22 14:34:11 +0700

    Lần thứ 1

---
+++ ThiGK2_NguyenHuuTrong_65133958/src/test/java/thigk2/nguyenhuutrong/ThiGk2NguyenHuuTrong65133958ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenhuutrong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiGk2NguyenHuuTrong65133958ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
