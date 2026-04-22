6bcf28d45c95558c9c08deda7f867c2ea67c675a
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-22 14:43:16 +0700

    Lần thứ 2

---
+++ ThiGK2_NguyenHuuTrong_65133958/src/main/java/thigk2/nguyenhuutrong/models/SanPham.java
@@ -0,0 +1,57 @@
+package thigk2.nguyenhuutrong.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "san_pham")
+public class SanPham {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String tenSanPham;
+	private Double gia;
+
+	@ManyToOne
+	@JoinColumn(name = "loai_id")
+	private LoaiSanPham loaiSanPham;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTenSanPham() {
+		return tenSanPham;
+	}
+
+	public void setTenSanPham(String tenSanPham) {
+		this.tenSanPham = tenSanPham;
+	}
+
+	public Double getGia() {
+		return gia;
+	}
+
+	public void setGia(Double gia) {
+		this.gia = gia;
+	}
+
+	public LoaiSanPham getLoaiSanPham() {
+		return loaiSanPham;
+	}
+
+	public void setLoaiSanPham(LoaiSanPham loaiSanPham) {
+		this.loaiSanPham = loaiSanPham;
+	}
+
+}
