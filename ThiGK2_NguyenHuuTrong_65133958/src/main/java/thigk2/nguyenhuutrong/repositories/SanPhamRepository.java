912708a2d639f70d763d023c4e20d7da40068617
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-22 14:50:12 +0700

    Lần thứ 3

---
+++ ThiGK2_NguyenHuuTrong_65133958/src/main/java/thigk2/nguyenhuutrong/repositories/SanPhamRepository.java
@@ -0,0 +1,8 @@
+package thigk2.nguyenhuutrong.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import thigk2.nguyenhuutrong.models.SanPham;
+
+public interface SanPhamRepository extends JpaRepository<SanPham, Long> {
+}
