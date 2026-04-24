1987308bfa919036253379f1c894f564e8ffcdf6
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-24 13:46:10 +0700

    Lần thứ 1

---
+++ ThiGiuaKy2_NguyenHuuTrong_65133958/src/main/java/thigk2/nguyenhuutrong/ThiGiuaKy2NguyenHuuTrong65133958Application.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenhuutrong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGiuaKy2NguyenHuuTrong65133958Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiGiuaKy2NguyenHuuTrong65133958Application.class, args);
+	}
+
+}
