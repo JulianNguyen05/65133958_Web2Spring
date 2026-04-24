15461d24cd2b40946b18775c83025647f5bda5bf
Julian Nguyen <huutrong.160705@gmail.com>
2026-04-24 14:27:44 +0700

    Lần thứ 7

--- ThiGiuaKy2_NguyenHuuTrong_65133958/src/main/java/thigk2/nguyenhuutrong/models/TheLoaiTin.java
+++ ThiGiuaKy2_NguyenHuuTrong_65133958/src/main/java/thigk2/nguyenhuutrong/models/TheLoaiTin.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class TheLoaiTin {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "name")
 	private String tenLoai;
 
 	@JsonIgnore
